8ac4177175afe73c46efd957613a234ee3b483bc
Bayron Castillo <bayroncastillo900725@gmail.com>
2024-11-14 20:42:33 -0500

    Añadiendo nuevas carpetas y archivos al proyecto

--- src/pages/Home/components/Hero.js
+++ src/pages/Home/components/Hero.js
@@ -9,7 +9,7 @@ export const Hero = () => {
             <Link to="/products" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Conoce Nuestros Servicios</Link>
         </div>
         <div className="visual my-5 lg:max-w-xl">
-            <img className="rounded-lg max-h-full" src="/images/hero_1.avif" alt="CodeBook Hero Section" />
+            <img className="rounded-lg max-h-full" src="https://edu-easy.netlify.app/images/blog/b2.webp" alt="CodeBook Hero Section" />
         </div>
     </section>
   )
